cfc310b953f8e01cd089458c962fbe0f67ab20c5
3duardonery <edu.nery.cordeiro@gmail.com>
2020-02-22 09:12:25 -0300

    Component lifecycles

--- src/components/TechList.js
+++ src/components/TechList.js
@@ -11,6 +11,26 @@ class TechList extends Component {
     ]
   };
 
+  // Executado assim que o componente aparece em tela
+  componentDidMount() {
+    const techs = localStorage.getItem('techs');
+    if (techs) {
+      this.setState({techs: JSON.parse(techs)});
+    }
+  }
+
+  // Executado assim que houver alterações nas props ou estado
+  componentDidUpdate(_, prevState) {
+    if (prevState.techs !== this.state.techs) {
+      localStorage.setItem('techs', JSON.stringify(this.state.techs));
+    }
+  }
+
+  // Executado quando o componente deixa de existir
+  componentWillMount() {
+
+  }
+
   handlerInputChange = e => {
     console.log(e.target.value);
     this.setState({ newTech: e.target.value });
